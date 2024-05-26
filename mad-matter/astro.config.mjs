8125b77863d3df7811d6b11bf477ed3b54af1ee4
Joseph Balsavich <balsa013@umn.edu>
2024-05-25 22:44:24 -0500

    addded netlify and sitemap optimizations

--- mad-matter/astro.config.mjs
+++ mad-matter/astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify/functions';
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  adapter: netlify(),
+  integrations: [sitemap()]
+});
