dbc837c4c660daf0d7e0f4826e4c18e4cb0140cd
Joseph Balsavich <balsa013@umn.edu>
2024-05-26 00:42:20 -0500

    removed netlify adapter

--- mad-matter/astro.config.mjs
+++ mad-matter/astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify/functions';
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: netlify(),
   integrations: [sitemap()]
 });
